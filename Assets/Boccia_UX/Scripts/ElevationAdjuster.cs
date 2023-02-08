f21f510e9ead1aa749e2469cdd7b6bdd0aa9caf4
b-lillywhite <brielle.lillywhite@ucalgary.ca>
2023-02-08 09:27:29 -0700

    Height Adjuster using Lerp
    - training text
    - added ramp reset in ball reset script
    - changed height adjuster to use lerp

--- Assets/Boccia_UX/Scripts/ElevationAdjuster.cs
+++ Assets/Boccia_UX/Scripts/ElevationAdjuster.cs
@@ -27,7 +27,7 @@ public class ElevationAdjuster : MonoBehaviour
         currentHeight = elevatorPlate.transform.position.z; //new
         targetHeight = currentHeight += change; //new and old
 
-        if (targetHeight < -0.051f)
+        if (targetHeight < -0.05204198f)
         {
             targetHeight = -0.05204198f;
         }
@@ -43,19 +43,20 @@ public class ElevationAdjuster : MonoBehaviour
         currentHeight = elevatorPlate.transform.position.z;
 
         float speed = heightSpeed;
-        if ((currentHeight - targetHeight) < -0.0015 & (currentHeight - targetHeight) > 0.00015)
+        if ((currentHeight - target) < -0.015 & (currentHeight - target) > 0.015)
         {
             speed = 0;
         }
         if (target < currentHeight)
         {
-            elevatorPlate.transform.Translate(Vector3.forward *speed* Time.deltaTime);
+            elevatorPlate.transform.Translate(Vector3.forward * speed * Time.deltaTime);
             //elevatorPlate.transform.position.z = currentHeight;
         }
         if (target > currentHeight)
         {
-            elevatorPlate.transform.Translate(Vector3.back * speed*Time.deltaTime);
+            elevatorPlate.transform.Translate(Vector3.back * speed * Time.deltaTime);
         }
+
 
         yield return null;
     }
@@ -63,13 +64,13 @@ public class ElevationAdjuster : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentHeight = elevatorPlate.transform.position.z;
+        //currentHeight = elevatorPlate.transform.position.z;
         Debug.Log(targetHeight + ":" + currentHeight);
 
         ////currentHeight = elevatorPlate.transform.position.z;
@@ -92,3 +93,75 @@ public class ElevationAdjuster : MonoBehaviour
     }
 
 }
+
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Unity;
+//using static UnityEngine.GraphicsBuffer;
+
+//public class ElevationAdjuster : MonoBehaviour
+//{
+//    public GameObject elevatorPlate;
+//    public float heightInc = 0.005f;
+//    public float heightSpeed = 5.0f; //10
+//    private float targetHeight;
+//    //float targetHeight = -0.03243253f;
+//    private float currentHeight;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        targetHeight = elevatorPlate.transform.position.y;
+//    }
+
+//    public void MoveUp()
+//    {
+//        changeHeight(heightInc);
+//    }
+
+//    public void MoveDown()
+//    {
+//        changeHeight(-heightInc);
+//    }
+
+//    public void changeHeight(float change)
+//    {
+//        //currentHeight = elevatorPlate.transform.position.z; //new
+
+//        if (targetHeight > 5f)
+//        {
+//            targetHeight = -0.05204198f;
+//        }
+//        if (targetHeight < 2.25f)
+//        {
+//            targetHeight = 2.25f;
+//        }
+
+//        targetHeight = targetHeight + change;
+
+//        //if (targetHeight < -0.05204198f)
+//        //{
+//        //    targetHeight = -0.05204198f;
+//        //}
+//        //if (targetHeight > -0.03243253f)
+//        //{
+//        //    targetHeight = -0.03243253f;
+//        //}
+//    }
+
+
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        currentHeight = elevatorPlate.transform.position.y;
+//        Debug.Log(targetHeight + ":" + currentHeight);
+
+//        //elevatorPlate.transform.position.y = Mathf.Lerp(currentHeight, targetHeight, heightSpeed * Time.deltaTime);
+
+//    }
+
+//}
