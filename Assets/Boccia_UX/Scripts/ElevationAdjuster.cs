63e73a04454700ceddfcc4d529c2c32c653bee57
b-lillywhite <brielle.lillywhite@ucalgary.ca>
2023-02-08 10:52:38 -0700

    Fixed Height Adjsuter
    - Changed to Lerp

--- Assets/Boccia_UX/Scripts/ElevationAdjuster.cs
+++ Assets/Boccia_UX/Scripts/ElevationAdjuster.cs
@@ -1,101 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Unity;
-using static UnityEngine.GraphicsBuffer;
-
-public class ElevationAdjuster : MonoBehaviour
-{
-    public GameObject elevatorPlate;
-    public float heightInc = 0.005f;
-    public float heightSpeed = 10.0f;
-    float targetHeight = -0.03243253f;
-    float currentHeight;
-
-    public void MoveUp()
-    {
-        changeHeight(-heightInc);
-    }
-
-    public void MoveDown()
-    {
-        changeHeight(heightInc);
-    }
-
-    public void changeHeight(float change)
-    {
-        currentHeight = elevatorPlate.transform.position.z; //new
-        targetHeight = currentHeight += change; //new and old
-
-        if (targetHeight < -0.05204198f)
-        {
-            targetHeight = -0.05204198f;
-        }
-        if (targetHeight > -0.03243253f)
-        {
-            targetHeight = -0.03243253f;
-        }
-        StartCoroutine("ChangeMyHeight", targetHeight);
-    }
-
-    public IEnumerator ChangeMyHeight(float target)
-    {
-        currentHeight = elevatorPlate.transform.position.z;
-
-        float speed = heightSpeed;
-        if ((currentHeight - target) < -0.015 & (currentHeight - target) > 0.015)
-        {
-            speed = 0;
-        }
-        if (target < currentHeight)
-        {
-            elevatorPlate.transform.Translate(Vector3.forward * speed * Time.deltaTime);
-            //elevatorPlate.transform.position.z = currentHeight;
-        }
-        if (target > currentHeight)
-        {
-            elevatorPlate.transform.Translate(Vector3.back * speed * Time.deltaTime);
-        }
-
-
-        yield return null;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //currentHeight = elevatorPlate.transform.position.z;
-        Debug.Log(targetHeight + ":" + currentHeight);
-
-        ////currentHeight = elevatorPlate.transform.position.z;
-
-        //float speed = heightSpeed;
-        ////if ((currentHeight - targetHeight) < -0.0015 & (currentHeight - targetHeight) > 0.15)
-        ////{
-        ////    speed = 0;
-        ////}
-        //if (targetHeight < currentHeight)
-        //{
-        //    elevatorPlate.transform.Translate(Vector3.forward * Time.deltaTime);
-        //    //elevatorPlate.transform.position.z = currentHeight;
-        //}
-        //if (targetHeight > currentHeight)
-        //{
-        //    elevatorPlate.transform.Translate(Vector3.forward * -Time.deltaTime);
-        //}
-
-    }
-
-}
-
-
-
 //using System.Collections;
 //using System.Collections.Generic;
 //using UnityEngine;
@@ -106,62 +8,154 @@ public class ElevationAdjuster : MonoBehaviour
 //{
 //    public GameObject elevatorPlate;
 //    public float heightInc = 0.005f;
-//    public float heightSpeed = 5.0f; //10
-//    private float targetHeight;
-//    //float targetHeight = -0.03243253f;
-//    private float currentHeight;
-
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        targetHeight = elevatorPlate.transform.position.y;
-//    }
+//    public float heightSpeed = 10.0f;
+//    float targetHeight = -0.03243253f;
+//    float currentHeight;
 
 //    public void MoveUp()
-//    {
-//        changeHeight(heightInc);
-//    }
-
-//    public void MoveDown()
 //    {
 //        changeHeight(-heightInc);
 //    }
 
+//    public void MoveDown()
+//    {
+//        changeHeight(heightInc);
+//    }
+
 //    public void changeHeight(float change)
 //    {
-//        //currentHeight = elevatorPlate.transform.position.z; //new
+//        currentHeight = elevatorPlate.transform.position.z; //new
+//        targetHeight = currentHeight += change; //new and old
 
-//        if (targetHeight > 5f)
+//        if (targetHeight < -0.05204198f)
 //        {
 //            targetHeight = -0.05204198f;
 //        }
-//        if (targetHeight < 2.25f)
+//        if (targetHeight > -0.03243253f)
 //        {
-//            targetHeight = 2.25f;
+//            targetHeight = -0.03243253f;
 //        }
-
-//        targetHeight = targetHeight + change;
-
-//        //if (targetHeight < -0.05204198f)
-//        //{
-//        //    targetHeight = -0.05204198f;
-//        //}
-//        //if (targetHeight > -0.03243253f)
-//        //{
-//        //    targetHeight = -0.03243253f;
-//        //}
+//        StartCoroutine("ChangeMyHeight", targetHeight);
 //    }
 
+//    public IEnumerator ChangeMyHeight(float target)
+//    {
+//        currentHeight = elevatorPlate.transform.position.z;
 
+//        float speed = heightSpeed;
+//        if ((currentHeight - target) < -0.015 & (currentHeight - target) > 0.015)
+//        {
+//            speed = 0;
+//        }
+//        if (target < currentHeight)
+//        {
+//            elevatorPlate.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+//            //elevatorPlate.transform.position.z = currentHeight;
+//        }
+//        if (target > currentHeight)
+//        {
+//            elevatorPlate.transform.Translate(Vector3.back * speed * Time.deltaTime);
+//        }
+
+
+//        yield return null;
+//    }
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+
+//    }
 
 //    // Update is called once per frame
 //    void Update()
 //    {
-//        currentHeight = elevatorPlate.transform.position.y;
+//        //currentHeight = elevatorPlate.transform.position.z;
 //        Debug.Log(targetHeight + ":" + currentHeight);
 
-//        //elevatorPlate.transform.position.y = Mathf.Lerp(currentHeight, targetHeight, heightSpeed * Time.deltaTime);
+//        ////currentHeight = elevatorPlate.transform.position.z;
+
+//        //float speed = heightSpeed;
+//        ////if ((currentHeight - targetHeight) < -0.0015 & (currentHeight - targetHeight) > 0.15)
+//        ////{
+//        ////    speed = 0;
+//        ////}
+//        //if (targetHeight < currentHeight)
+//        //{
+//        //    elevatorPlate.transform.Translate(Vector3.forward * Time.deltaTime);
+//        //    //elevatorPlate.transform.position.z = currentHeight;
+//        //}
+//        //if (targetHeight > currentHeight)
+//        //{
+//        //    elevatorPlate.transform.Translate(Vector3.forward * -Time.deltaTime);
+//        //}
 
 //    }
 
 //}
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity;
+using static UnityEngine.GraphicsBuffer;
+
+public class ElevationAdjuster : MonoBehaviour
+{
+    public GameObject elevatorPlate;
+    public float heightInc = 0.005f;
+    public float heightSpeed = 5.0f; //10
+    public Vector3 targetHeight;
+    //float targetHeight = -0.03243253f;
+    public Vector3 currentHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetHeight = elevatorPlate.transform.localPosition;
+    }
+
+    public void MoveUp()
+    {
+        targetHeight = targetHeight + Vector3.forward * heightInc;
+        changeHeight(targetHeight.z);
+        //changeHeight(heightInc);
+    }
+
+    public void MoveDown()
+    {
+        targetHeight = targetHeight + Vector3.back * heightInc;
+        changeHeight(targetHeight.z);
+        //changeHeight(-heightInc);
+    }
+
+    public void changeHeight(float change)
+    {
+        //targetHeight = targetHeight + Vector3.up * heightInc;
+
+        if (targetHeight.z > 0.045f)
+        {
+            targetHeight.z = 0.045f;
+        }
+        if (targetHeight.z < 0.0f)
+        {
+            targetHeight.z = 0.0f;
+        }
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentHeight = elevatorPlate.transform.localPosition;
+        Debug.Log(targetHeight + ":" + currentHeight);
+
+        elevatorPlate.transform.localPosition = Vector3.Lerp(currentHeight, targetHeight, heightSpeed * Time.deltaTime);
+
+    }
+
+}
