0be09c10dece1dd047a0a50703d37200871d4a8d
MuhammadFAns <118628021+MuhammadFAns@users.noreply.github.com>
2023-08-10 12:19:43 -0600

    Added serial monitor connection

--- Assets/Boccia_UX/Scripts/SerialButtonToggle.cs
+++ Assets/Boccia_UX/Scripts/SerialButtonToggle.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SerialButtonToggle : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Text buttonText;
+    public TextMeshProUGUI buttonText;
     private bool isOn = false;
 
 
     void Start()
     {
-    Button button = GameObject.Find("SerialMonitorToggle").GetComponent<Button>();
-    button.onClick.AddListener(ToggleButton);
+
+        Button button = this.gameObject.GetComponent<Button>();
+        button.onClick.AddListener(ToggleButton);
+    
     }
 
     // Update is called once per frame
@@ -21,7 +25,7 @@ public class SerialButtonToggle : MonoBehaviour
         
     }
 
-    private void ToggleButton()
+    public void ToggleButton()
     {
         isOn = !isOn;
 
@@ -33,37 +37,6 @@ public class SerialButtonToggle : MonoBehaviour
         else
         {
             buttonText.text = "Serial Monitor: Off";
-            // You can perform other actions when the button is turned off
-        }
-    }
-}
-
-using UnityEngine;
-using UnityEngine.UI;
-
-public class ButtonToggle : MonoBehaviour
-{
-    public Text buttonText;
-    private bool isOn = false;
-
-    private void Start()
-    {
-        Button button = GetComponent<Button>();
-        button.onClick.AddListener(ToggleButton);
-    }
-
-    private void ToggleButton()
-    {
-        isOn = !isOn;
-
-        if (isOn)
-        {
-            buttonText.text = "On";
-            // You can perform other actions when the button is turned on
-        }
-        else
-        {
-            buttonText.text = "Off";
             // You can perform other actions when the button is turned off
         }
     }
